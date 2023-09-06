edf7bb5be2b75689cf846e31ba71da4cbf22db9a
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2023-09-06 13:36:30 +0100

    Update main.tf

--- single-project/terraform/main.tf
+++ single-project/terraform/main.tf
@@ -1,3 +1,26 @@
 resource "random_pet" "this" {
   length = 5
 }
+
+resource "aws_s3_bucket" "exampletestingtginfracost" {
+  bucket = "exampletestingtginfracost"
+
+  tags = {
+    Name        = "exampletestingtginfracost"
+    Environment = "Dev"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0" # Consider specifying a version constraint for better stability.
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "default"
+}
